8d677dcdebf1198f6a6c42854f989f6a72839363
Carlosmax1 <carlosmaximoinsta@gmail.com>
2022-09-15 12:15:22 -0300

    config mini cursos logica

--- src/components/MiniCourses/index.js
+++ src/components/MiniCourses/index.js
@@ -22,40 +22,114 @@ import {
 } from './styleMiniCourses';
 import { mini_courses } from '../../utils/mini-courses';
 
+let disabled = [];
+let selected = [];
+
 export default function MiniCoursesC({ courses }) {
 
-  const [selected, setSelected] = useState({});
-  let list = [];
+  const [blocked, setBlocked] = useState({});
 
-  function chosen(id){
+  function chosen(course) {
+    console.log(selected);
+    if (selected.length === 0) {
+      selected.push(course);
+      console.log("Push Course = ", selected);
+    } else {
+      console.log("Entrou");
+      selected.splice(selected.indexOf(course));
+      disabled = [];
+    }
+    selection();
+  }
+
+  function click(id) {
     const course = mini_courses.find(course => course.id === id);
-    if(list.indexOf(course) !== -1){
-      list.splice(list.indexOf(course));
-      //console.log("removeu");
-    }else{
-      list.push(course);
-      //console.log("adicionou");
-    }
-    console.log(list);
+    chosen(course);
+    console.log("clicou");
+    console.log("Selected = ", selected);
+    console.log("Disabled = ", disabled);
   }
 
-  function click(id){
-    chosen(id);
-    if(list.length === 1){
-      if(list[0].time !== ''){
+  console.log("Blocked = ", blocked);
 
+  function selection() {
+    let i;
+    let j;
+    //let disabled = [];
+    for (i = 0; i < mini_courses.length; i++) {
+      for (j = 0; j < selected.length; j++) {
+        if ((tradeOff(mini_courses[i], selected[j])) && (mini_courses[i] !== selected[j])) {
+          disabled.push(mini_courses[i]);
+        }
       }
     }
+    disabledVerify(disabled);
   }
 
-  function tradeOff(obj1, obj2){
+  function disabledVerify(disabled) {
+    let aux = {};
+    for (let i = 1; i <= mini_courses.length; i++) {
+      aux[i] = false;
+    }
+    disabled.forEach(item => {
+      aux[item.id] = true;
+    })
+    setBlocked(aux);
+  }
+
+  function tradeOff(obj1, obj2) {
     let listObj = [obj1.time, obj2.time];
-    for(let i = 0; i < 2; i++){
-      if(listObj[i].slice(3) === '00'){
-        break;
+    let aux = [];
+    let a = 0, b = '';
+    //console.log("Não ordenado = ",listObj);
+    listObj.sort((a, b) => {
+      if (a < b) {
+        return -1;
+      } else {
+        return true;
+      }
+    });
+    //console.log("Ordenado = ",listObj);
+    /* 
+      10:00 09:30 and 10:30
+      09:30 10:00 and 09:00
+
+      10:30 com 11:00
+    */
+
+    a = parseInt(listObj[0].slice(0, 2));
+    b = parseInt(listObj[0].slice(3));
+    if (b === 30) {
+      aux.push(String(a) + ':00');
+      a = a + 1;
+      if (a < 10) {
+        a = '0' + String(a);
+      } else {
+        a = String(a);
+      }
+      aux.push(a + ':00');
+    } else {
+      aux.push(String(a) + ':30');
+      a = a - 1;
+      if (a < 10) {
+        a = '0' + String(a);
+      } else {
+        a = String(a);
+      }
+      aux.push(a + ':30');
+    }
+
+    console.log("Aux tradeoff = ", aux);
+    console.log("List object tradeoff = ", listObj);
+    for (let i = 0; i < aux.length; i++) {
+      if (aux[i] === listObj[1] && aux[i] !== listObj[0]) {
+        console.log("Houve conflito");
+        return true;
       }
     }
-  } 
+    console.log("Não houve conflito");
+    return false;
+  }
 
   return (
     <>
@@ -79,7 +153,11 @@ export default function MiniCoursesC({ courses }) {
                     <InfosContainer key={index}>
                       <RadioContainer>
                         <BoxRadio>
-                          <Radio disabled={selected[item.id]} onClick={() => click(item.id)} type='checkbox' />
+                          <Radio
+                            disabled={blocked[item.id]}
+                            onClick={() => click(item.id)}
+                            type='checkbox'
+                          />
                         </BoxRadio>
                         <NameCourse>{item.name}</NameCourse>
                       </RadioContainer>
--- src/utils/mini-courses.js
+++ src/utils/mini-courses.js
@@ -20,6 +20,27 @@ const mini_courses = [
     sala: "LAE 001",
     time: '10:00',
   },
+  {
+    id: 4,
+    name: "teste",
+    spots: 50,
+    sala: "LAE 001",
+    time: '10:30',
+  },
+  {
+    id: 5,
+    name: "teste",
+    spots: 50,
+    sala: "LAE 001",
+    time: '11:00',
+  },
+  {
+    id: 6,
+    name: "teste",
+    spots: 50,
+    sala: "LAE 001",
+    time: '11:30',
+  }
 ]
 
 export { mini_courses };
